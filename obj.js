64810be12090c9d86071891dc4369dd24f9ad0fe
P S Shrijith <shrijithps89@gmail.com>
2023-07-02 22:01:44 +0530

    objects

---
+++ obj.js
@@ -0,0 +1,31 @@
+/* Write your code below. Good luck! 🙂 */
+
+let mark = {
+    fullName:"Mark Miller",
+    mass:78,
+    height:1.69,
+    
+    calcBMI : function(){
+        this.bmi = this.mass/(this.height*this.height);
+        return this.bmi;
+    }
+}
+
+let john = {
+    fullName:"John Smith",
+    mass:92,
+    height:1.95,
+    
+    calcBMI : function(){
+        this.bmi = this.mass/(this.height*this.height);
+        return this.bmi;
+    }
+}
+
+
+if(mark.calcBMI()>john.calcBMI()){
+    console.log(`${mark.fullName}'s BMI ${(mark.calcBMI())} is higher than ${john.fullName}'s ${(john.calcBMI())}`)
+}
+else{
+    console.log(`${john.fullName}'s BMI ${(john.calcBMI())} is higher than ${mark.fullName}'s ${(mark.calcBMI())}`)
+}
